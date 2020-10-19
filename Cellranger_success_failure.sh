6172e4421a6b2dd669215e09315166cd4c9d785e
EnJun Yang <56063889+EnJun-Yang@users.noreply.github.com>
2020-10-19 16:17:02 +0800

    Update Cellranger_success_failure.sh

--- Cellranger_success_failure.sh
+++ Cellranger_success_failure.sh
@@ -27,6 +27,7 @@ k8jobname="$expidlower"-"$idlower"-cellranger-"$BUILDID";
 #if ([cellranger count --id=$id --transcriptome=$TRANSCRIPTOME --sample=$SAMPLE --fastqs=$FASTQS] && [ls] && [gsutil cp -r $id gs://testinggenomic/Cellranger_output]);
 #if cellranger count --id=$id --transcriptome=$TRANSCRIPTOME --sample=$SAMPLE --fastqs=$FASTQS && ls && gsutil cp -r $id gs://testinggenomic/Cellranger_output ;
 echo "cellranger count --id=$id --transcriptome=$TRANSCRIPTOME --sample=$SAMPLE --fastqs=$FASTQS && ls && gsutil cp -r $id gs://testinggenomic/Cellranger_output";
+ls $FASTQs;
 if cellranger testrun --id=tiny ;
 then
 echo "Success"
